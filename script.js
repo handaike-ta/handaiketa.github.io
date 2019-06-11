40f9b1f14d0f66e22cb004f6826bda5bd10e9ca4
handaike-ta <50145556+handaike-ta@users.noreply.github.com>
2019-06-12 00:56:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,5 @@ $(function() {
           scrollTop:0
         },500);
       });
-    $('.modal-top a').click(function(){
-        $('.modal-top a')css('background-color': '#DCE600');
-      });
+
 });
